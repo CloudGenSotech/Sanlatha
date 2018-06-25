c45254eede938a0cf03eaa128af4d1a258432a9b
ThotaThanish <sriurr@gmail.com>
2018-06-25 21:44:09 +0530

    testfile1

---
+++ testfile.java
@@ -0,0 +1,13 @@
+class testfile
+{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		    a=60;
+		b=400;
+		c=250;
+		c=a+b+c;
+		System.out.println("The sum of three values are "+c);
+	}
+}
+
